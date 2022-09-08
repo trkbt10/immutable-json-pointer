c6f17ce3779f4901b2fecf5f33a88273e8b2882a
Teruchiki <t>
2022-09-08 14:31:59 +0900

    Update 1.0.0 -> 1.0.1

--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -1,10 +1,12 @@
 declare type JSONPointerReplacer = (value: any, key: string, parent: any, paths: string[]) => void;
-export declare function resolver<T extends {}, V extends unknown, P extends string>(doc: T, pointer: P, callback?: JSONPointerReplacer): V;
-export declare function toPointer(paths: (string | number)[]): string;
+export declare function escape(str: string): string;
+export declare function unescape(str: string): string;
+export declare function parse(pointer: string): string[];
+export declare function has<T extends {}>(obj: T, pointer: string): boolean;
+export declare function compile(paths: (string | number)[]): string;
+export declare function resolve<T extends {}, V extends unknown, P extends string>(doc: T, pointer: P, callback?: JSONPointerReplacer): V;
 export declare function get<T extends {}, R extends unknown>(json: T, pointer: string): R;
+export declare function remove<T extends {}>(json: T, pointer: string): T;
+export declare function dict<T extends {}>(json: T): Record<string, unknown>;
 export declare function set<T extends {}>(doc: T, pointer: string, nextValue: any): T;
-export declare function compile<T extends {}, R extends unknown>(pointer: string): {
-    get: (doc: T) => R;
-    set: (doc: T, nextValue: any) => T;
-};
 export {};
