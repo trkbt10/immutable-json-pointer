189dd90c63442b25726deab92da1c2769eb875d9
Teruchiki <t>
2022-09-30 14:56:50 +0900

    Fix remove function

---
+++ __tests__/remove.spec.ts
@@ -0,0 +1,25 @@
+import { remove } from "../src";
+
+describe("Remove", () => {
+  // For example, given the JSON document
+  it("Basic", () => {
+    const json = {
+      foo: [0, 1, 2, 3, 4, 5],
+      hoge: "fuga",
+    };
+    expect(remove(json, "/foo/0")).toStrictEqual({
+      foo: [1, 2, 3, 4, 5],
+      hoge: "fuga",
+    });
+    expect(remove(json, "/foo/1")).toStrictEqual({
+      foo: [0, 2, 3, 4, 5],
+      hoge: "fuga",
+    });
+    expect(remove(json, "/foo")).toEqual({
+      hoge: "fuga",
+    });
+    expect(remove(json, "/hoge")).toEqual({
+      foo: [0, 1, 2, 3, 4, 5],
+    });
+  });
+});
