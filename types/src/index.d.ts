254c41d9ba28d4d7959e319ebfa9e6c83c5e6b76
Teruchiki <t>
2023-07-07 12:57:42 +0900

    chore: Update package version to "1.0.7"

--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -8,6 +8,7 @@ export declare function resolve<T extends {}, V extends unknown, P extends strin
 export declare function get<T extends {}, R extends unknown>(json: T, pointer: string): R;
 export declare function remove<T extends {}>(json: T, pointer: string): T;
 export declare function dict<T extends {}>(json: T): Record<string, unknown>;
+export declare function compose<T extends {}, Initial extends T = T, Result extends any = T>(entriesOrRecord: [string, any][] | Record<string, unknown>, initial?: Initial): Result;
 export declare function set<T extends {}>(doc: T, pointer: string, nextValue: any): T;
 export declare function read<T extends {}>(json: T, pointer: string): unknown;
 export declare function transform<T extends {}, Fn extends (params: unknown) => unknown>(doc: T, pointer: string, fn: Fn): T;
